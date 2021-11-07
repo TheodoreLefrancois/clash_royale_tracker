15b062ba96a7bf4a3274153f5295c2182759da76
TheodoreLefrancois <theodore.lefrancois2906@gmail.com>
2021-11-07 19:10:09 +0100

    add the footer with the current version of the app and the author/contributors

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import GetPlayerData from "./components/GetPlayerData.vue";
+import Footer from "./components/layout/Footer.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div id="flex-root">
+    <GetPlayerData />
+    <Footer />
+  </div>
 </template>
 
 <style>
@@ -17,5 +18,12 @@ import HelloWorld from "./components/HelloWorld.vue";
   text-align: center;
   color: #aec2d3;
   margin-top: 60px;
+  height: 100%;
+}
+#flex-root {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/GetPlayerData.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+console.log("it works");
+</script>
+<template>
+  <h1>Here you can find datas about the player you are looking for</h1>
+  <div>
+    <label for="player-id">Write here the id of the player</label>
+    <input name="player-id" type="text" />
+  </div>
+</template>
+<style scoped>
+h1 {
+  font-size: 30px;
+  color: brown;
+}
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+label {
+  color: burlywood;
+  margin-bottom: 5px;
+}
+input {
+  height: 20px;
+  width: 100px;
+  color: darkcyan;
+  background-color: darkblue;
+  border-color: darkblue;
+}
+input:focus {
+  outline: 2px solid transparent;
+  outline-offset: 2px;
+  border-radius: 8;
+}
+</style>
