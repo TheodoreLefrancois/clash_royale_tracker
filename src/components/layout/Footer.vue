dce7479c57cd0fc20541c705fe56e41cca179b38
TheodoreLefrancois <theodore.lefrancois2906@gmail.com>
2022-01-19 19:38:40 +0100

    fix footer with the contributors and the position at the bottom of the page

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,44 +1,59 @@
 <script setup lang="ts">
-interface Person {
-  name: string;
-  email: string;
-  url: string;
-}
+import { ref } from "vue";
 import { version, author, contributors } from "../../../package.json";
+const thanked = ref(contributors.slice(0, 3));
 </script>
 
 <template>
-  <div>
+  <footer>
     <p>
       Made with &#10084;&#65039; by
       <a target="_blank" :href="author.url">{{ author.name }}</a>
     </p>
-    <p>
+    <p id="contributor-mapping">
       Thanks to
-      <a :href="contributors[0].url">{{ contributors[0].name }}</a>
+      <a v-for="(contributor, index) in thanked" :href="contributor.url">{{
+        index !== thanked.length - 1
+          ? contributor.name + "," + ` `
+          : contributor.name
+      }}</a>
+      <a
+        v-if="contributors.length !== thanked.length"
+        target="_blank"
+        href="https://github.com/TheodoreLefrancois/scrapp_royaleapi/graphs/contributors"
+      >
+        and more...
+      </a>
     </p>
     <p>
       Version : <span>{{ version }}</span>
     </p>
-  </div>
+  </footer>
 </template>
 
 <style scoped>
-template {
+footer {
   z-index: 10;
-  position: fixed;
-  bottom: 0%;
+  position: absolute;
+  bottom: 0;
   width: 100%;
-  background-color: darkgreen;
-  height: 100px;
+  background-color: crimson;
   color: darkgrey;
   text-align: center;
 }
 span {
   font-style: italic;
-  color: crimson;
+  color: darkblue;
+}
+#contributor-mapping {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 }
 a {
   text-decoration: none;
+  padding-left: 5px;
+  padding-right: 5px;
+  color: darkblue;
 }
 </style>
