50e71309e82a7c9d192bd97e0f635cbee23bef4e
TheodoreLefrancois <theodore.lefrancois2906@gmail.com>
2022-01-21 18:57:49 +0100

    plugin is now functional so I restored tests

--- src/tests/app.spec.ts
+++ src/tests/app.spec.ts
@@ -4,5 +4,5 @@ import { mount } from "@vue/test-utils";
 test("test App.vue", async () => {
   expect(App).toBeTruthy();
   const wrapper = mount(App);
-  expect(wrapper.attributes("test-id")).toBe("root-di");
+  expect(wrapper.attributes("test-id")).toBe("root-div");
 });
