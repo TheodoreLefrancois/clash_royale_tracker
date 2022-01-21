6b57f9de7223b55342c2676ee52889ea9b3441f0
TheodoreLefrancois <theodore.lefrancois2906@gmail.com>
2022-01-21 18:35:43 +0100

    add tests to the netlify prebuild plugin

--- plugins/index.js
+++ plugins/index.js
@@ -1,5 +1,19 @@
+const { exec } = require("child_process");
+
 module.exports = {
   onPreBuild: () => {
-    console.log("Exectuted before build");
+    console.log("Exectuted tests before build");
+    exec("yarn test", (err, stdout, stderr) => {
+      console.log({
+        err,
+        stdout,
+        stderr,
+      });
+      if (err || stderr) {
+        throw new Error("Tests failed");
+      } else if (stdout) {
+        console.log("Tests passed");
+      }
+    });
   },
 };
