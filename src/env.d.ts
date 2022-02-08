094fbc58d5e5635df7a5b1e2a5cf8e7147ea8291
TheodoreLefrancois <theodore.lefrancois2906@gmail.com>
2022-02-08 23:30:29 +0100

    add intellisense for env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="vite/client" />
 
+// https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
+interface ImportMetaEnv {
+  readonly VITE_TOKEN: string;
+  readonly VITE_TOKEN_PROXY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 declare module "*.vue" {
   import { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
