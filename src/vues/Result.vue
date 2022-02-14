1dbed2fc8c4fbb43c8019dde219dfdc8e961e392
TheodoreLefrancois <theodore.lefrancois2906@gmail.com>
2022-02-14 23:49:46 +0100

    display data from clash royale api

--- src/vues/Result.vue
+++ src/vues/Result.vue
@@ -1,101 +1,154 @@
-<script setup lang="ts">
+<script lang="ts">
 import axios from "axios";
+import { defineComponent } from "vue";
 import { RoyaleResponse } from "../types";
-// const token = import.meta.env.VITE_TOKEN_PROXY;
-const token = import.meta.env.VITE_TOKEN;
-const { tag } = defineProps<{ tag: string }>();
-const Authorization = "Authorization";
-// const url = `https://proxy.royaleapi.dev/v1/players/%23${tag}`;
-const url = `https://api.clashroyale.com/v1/players/%23${tag}`;
-const { data } = await axios.get<RoyaleResponse>(url, {
-  headers: {
-    [Authorization]: `Bearer ${token}`,
+const token = import.meta.env.VITE_TOKEN_PROXY;
+const Component = defineComponent({
+  name: "Result",
+  props: {
+    tag: String,
   },
-});
-console.log(data);
-</script>
-
-<template>
-  <h1>Here you will find the result of the player</h1>
-  <h2>
-    You searched <span>{{ tag }}</span>
-  </h2>
-</template>
-
-<style scoped>
-h1 {
-  color: crimson;
-}
-</style>
-
-<!-- <script setup lang="ts">
-import axios from "axios";
-import { RoyaleResponse } from "../types";
-import { defineComponent, onMounted, ref } from "vue";
-const token = import.meta.env.VITE_TOKEN;
-const { tag } = defineProps<{ tag: string }>();
-defineComponent({
-  props: {},
-  name: "Result Vue",
-  setup() {
-    const data = ref<RoyaleResponse>({} as RoyaleResponse);
-    const loading = ref(true);
-    const error = ref(null);
-
-    async function fetchData() {
-      const url = `https://proxy.royaleapi.dev/v1/players/%23${tag}`;
-      const response = await axios.get<RoyaleResponse>(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      console.log(response);
-      data.value = response.data;
-      loading.value = false;
-    }
-    onMounted(() => {
-      fetchData();
-    });
-    on;
-    return {
-      data,
-      loading,
-      error,
-    };
+  setup(props) {
+    props.tag;
   },
-});
-export default {
   data() {
     return {
-      data: {},
+      loading: true,
+      data: {} as RoyaleResponse,
     };
   },
-  mounted() {
-    const url = `https://proxy.royaleapi.dev/v1/players/%23${tag}`;
-    const response = axios
-      .get<RoyaleResponse>(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
+  computed: {
+    badges() {
+      return [
+        {
+          name: "grandChallenge",
+          logoUrl: "/src/assets/grandChallengeUrl.jpeg",
+          value:
+            this.data.badges.find((badge) => badge.name === "Grand12Wins")
+              ?.progress || 0,
         },
-      })
-      .then((data) => (this.data = data));
-    console.log(response);
-    data.value = response.data;
-    loading.value = false;
+        {
+          name: "classicalChallenge",
+          logoUrl: "/src/assets/classicalChallengeUrl.jpeg",
+          value:
+            this.data.badges.find((badge) => badge.name === "Classic12Wins")
+              ?.progress || 0,
+        },
+        {
+          name: "bestTrophies",
+          logoUrl: "/src/assets/bestTrophiesUrl.jpeg",
+          value: this.data.bestTrophies,
+        },
+        {
+          name: "trophies",
+          logoUrl: "/src/assets/trophiesUrl.jpeg",
+          value: this.data.trophies,
+        },
+        {
+          name: "crl",
+          logoUrl: "src/assets/crlUrl.png",
+          value: this.data.badges
+            .filter((badge) => badge.name.startsWith("Crl20Wins"))
+            .map((crlBadge) => crlBadge.progress)
+            .join(" - "),
+        },
+        {
+          name: "topLadder",
+          logoUrl: "/src/assets/trophiesUrl.jpeg",
+          value: this.data.badges
+            .filter((badge) => badge.name.startsWith("LadderTop1000"))
+            .map((ladderBadge) => ladderBadge.progress)
+            .join(" - "),
+        },
+        {
+          name: "globalTournament",
+          logoUrl: "src/assets/globalTournamentUrl.jpeg",
+          value: this.data.badges
+            .filter((badge) => badge.name.startsWith("LadderTournamentTop1000"))
+            .map((globalTournamentBadge) => globalTournamentBadge.progress)
+            .join(" - "),
+        },
+        {
+          name: "year",
+          logoUrl: "src/assets/yearUrl.png",
+          value: this.data.badges.filter((badge) =>
+            badge.name.startsWith("Played")
+          ).length,
+        },
+      ];
+    },
   },
-};
+  mounted(): void {
+    axios
+      .get<RoyaleResponse>(
+        `https://proxy.royaleapi.dev/v1/players/%23${this.tag}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        this.data = res.data;
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  },
+});
+
+export default Component;
 </script>
 
 <template>
-  <h1>Here you will find the result of the player</h1>
-  <h2>
+  <h1>
     You searched <span>{{ tag }}</span>
-  </h2>
-  <div v-if=""></div>
+  </h1>
+  <h2 v-if="loading">Collecting data ...</h2>
+  <section v-if="data.role">
+    <h3>{{ data.name }}</h3>
+    <table>
+      <tr v-for="badge in badges">
+        <td>{{ badge.value }}</td>
+        <td>
+          <img :src="badge.logoUrl" :alt="badge.name" />
+        </td>
+      </tr>
+    </table>
+  </section>
 </template>
 
 <style scoped>
 h1 {
   color: crimson;
 }
-</style> -->
+section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+table {
+  border: solid 3px darkblue;
+  max-width: 700px;
+  width: 100%;
+}
+tr {
+  border: solid 3px darkblue;
+}
+td {
+  border: solid 3px darkblue;
+}
+img {
+  height: 30px;
+  width: 30px;
+}
+h3 {
+  font-size: large;
+}
+</style>
